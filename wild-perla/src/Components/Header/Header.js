359e346bcfe44a9e0952f8123ad1dff905376de0
Matt Gasper <mrgasper27@gmail.com>
2019-01-16 12:46:42 -0600

    Small style change and deploy to Firebase

--- wild-perla/src/Components/Header/Header.js
+++ wild-perla/src/Components/Header/Header.js
@@ -49,7 +49,7 @@ class Header extends React.Component  {
             {/* Facebook/Instagram links */}
             <NavbarEnd>
                 <NavbarItem href="https://www.facebook.com/wildperla/" target="_blank" isHidden='touch'>
-                    <Icon className='fab fa-facebook-f' />
+                    <Icon className='fab fa-facebook-f navIcon' />
                 </NavbarItem>
                 <NavbarItem href="http://instagram.com/wildperla" target="_blank" isHidden='touch'>
                     <Icon className='fab fa-instagram' />
